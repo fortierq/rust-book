6a8710a1f356ebea3359cd221d00b00b5b5ebc83
Quentin Fortier <qpfortier@gmail.com>
2021-01-24 21:36:22 +0100

    Add dichotomy algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,22 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-
-fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(x) => x,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+fn dicho(e: i32, array: &[i32]) -> bool {
+    let mut i = 0;
+    let mut j = array.len() - 1;
+    while i < j {
+        let m = (i + j) / 2;
+        if array[m] < e {
+            i = (m + 1) as usize;
+        } else {
+            j = m as usize;
         }
-    }
+    };
+    return array[j] == e;
+}
+fn main() {
+    let a = [0, 1, 4, 7, 8, 11];
+    let b = [-5, 2, 3, 7, 9];
+    assert_eq!(dicho(5, &a), false);
+    assert_eq!(dicho(1, &a), true);
+    assert_eq!(dicho(0, &a), true);
+    assert_eq!(dicho(8, &b), false);
+    assert_eq!(dicho(9, &a), true);
 }
